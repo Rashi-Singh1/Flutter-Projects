c808f12b8a9122345f9719d9a5afd1f3721bb6b2
Rashi Singh <rashi170101052@iitg.ac.in>
2019-12-08 04:47:12 +0530

    Asset images added

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -9,8 +9,9 @@ void main() {
     ),
     body: Center(
       child: Image(
-        image: NetworkImage(
-            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+//        image: NetworkImage(
+//            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+        image: AssetImage('images/diamond.png'),
       ),
     ),
     backgroundColor: Colors.blueGrey[10],
