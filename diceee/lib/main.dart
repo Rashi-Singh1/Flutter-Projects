1b24f0f7d800592884fb80fe02e8367be7b957cc
Rashi Singh <rashi170101052@iitg.ac.in>
2019-12-10 03:14:54 +0530

    Randomized gambling dicee completed

--- diceee/lib/main.dart
+++ diceee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,7 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: Dicepage(),
+        body: Dice(),
       ),
     ),
   );
@@ -25,20 +27,73 @@ class Dicepage extends StatelessWidget {
         Expanded(
           //to utilize the space by filling jitna possible hai
 //            flex: 2,    //to specify ratio when multiple expanded in a row
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+          child: FlatButton(
+            onPressed: () {
+              //this is a void anonymous function which does not accept parameters
+              print('Left Button was pressed');
+            },
             child: Image.asset('images/dice1.png'),
           ),
         ),
         Expanded(
 //            flex: 1,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+          child: FlatButton(
+            onPressed: () {
+              print('Right Button was pressed');
+            },
             child: Image.asset('images/dice6.png'),
           ),
-//            child: Image(
-//              image: AssetImage('images/dice2.png'),
-//            ), //another way to add image
+        ),
+      ],
+    );
+  }
+}
+
+//Stateless Widget is used if the stuff on screen won't change on user interaction
+// therefore is app ke liye stateful widget would be better, since tap pe image would change
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() =>
+      _DiceState(); //constructor for the class _DiceState
+  //rather than build called here, it returns the State after creating one
+}
+
+class _DiceState extends State<Dice> {
+  int leftdice = 1, rightdice = 1;
+
+  void setNumber() {
+    setState(() {
+      leftdice = new Random().nextInt(6) + 1;
+      print('leftdice no. is $leftdice');
+      rightdice = new Random().nextInt(6) + 1;
+      print('rightdice no. is $rightdice');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment
+          .stretch, //rather than this, can wrap with a center widget
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              //this is a void anonymous function which does not accept parameters
+              print('Left Button was pressed');
+              setNumber();
+            },
+            child: Image.asset('images/dice$leftdice.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              print('Right Button was pressed');
+              setNumber();
+            },
+            child: Image.asset('images/dice$rightdice.png'),
+          ),
         ),
       ],
     );
