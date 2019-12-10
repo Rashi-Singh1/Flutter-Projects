232af196f0d784ce3294a20f313c05bdfe138494
Rashi Singh <rashi170101052@iitg.ac.in>
2019-12-10 18:01:24 +0530

    Adding packages

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart'; //to add new package, add dependency, run get package and then add import command
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +9,9 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Text(nouns.last), //can check this from external libraries
+          ),
         ),
       ),
     );
