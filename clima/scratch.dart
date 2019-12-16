fe885bf842a400e382f728527e86d40955b01d78
Rashi Singh <rashi170101052@iitg.ac.in>
2019-12-17 04:38:13 +0530

    Future with async and await

--- clima/scratch.dart
+++ clima/scratch.dart
@@ -2,10 +2,11 @@ void main() {
   performTasks();
 }
 
-void performTasks() {
+void performTasks() async {
   task1();
-  String cur = task2();
+  String cur = await task2();
   task3(cur);
+  task4();
 }
 
 void task1() {
@@ -13,12 +14,13 @@ void task1() {
   print('Task 1 complete');
 }
 
-String task2() {
+Future task2() async {
   Duration threeseconds = Duration(seconds: 3);
   String result;
-  Future.delayed(threeseconds, () {
+
+  await Future.delayed(threeseconds, () {
     //this is an async function that can run in background and the function inside it will run after the said task is complete
-    //this is basically a function that occurs after the given duration (creates a future)
+    //this is basically allows a function to occurs after the given duration (creates a future), while next stmts are allowed to be executed
     result = 'task 2 data';
     print('Task 2 complete');
   });
@@ -29,4 +31,9 @@ String task2() {
 void task3(String task2Data) {
   String result = 'task 3 data';
   print('Task 3 complete with $task2Data');
+}
+
+void task4() {
+  String result = 'task 4 data';
+  print('Task 4 complete');
 }
