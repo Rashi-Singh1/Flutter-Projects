b4aa18317a93653f59e0fa7e265ab096b99830cd
Rashi Singh <rashi170101052@iitg.ac.in>
2019-12-25 03:28:24 +0530

    Synced the Change Notifier with the Provider Package

--- todoey/lib/scratch.dart
+++ todoey/lib/scratch.dart
@@ -5,14 +5,19 @@ class MyApp extends StatelessWidget {
   final String data = 'Top Secret data';
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: (context) => data,
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
             title: Text(data),
           ),
-          body: Level1(),
+          body: Column(
+            children: <Widget>[
+              MyText(),
+              Level1(),
+            ],
+          ),
         ),
       ),
     );
@@ -30,15 +35,61 @@ class Level1 extends StatelessWidget {
 class Level2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Level3();
+    return Column(
+      children: <Widget>[
+        MyTextField(),
+        Level3(),
+      ],
+    );
   }
 }
 
 class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(Provider.of(context));
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //this would allow the Text to be initialized with the value of data, but not be rebuild, every time data is changed
+    return Text(Provider.of<Data>(context, listen: false).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (value) {
+        //TODO: move this value to data inside Data class
+        Provider.of<Data>(context).changeString(value);
+      },
+    );
+  }
+}
+
+//to notify listeners of update in this class
+//ChangeNotifier does not come with Provider Package
+class Data extends ChangeNotifier {
+  String data = 'Some data';
+
+  void changeString(String newString) {
+    data = newString;
+
+    //Very Very imp...for change in the data being listened
+    notifyListeners();
   }
 }
 
 void main() => runApp(MyApp());
+
+//For this structure...need to update the state using Provider Package
+//                                   MyApp
+//            myText                                    Level1
+//
+//                                                      Level2
+//                                        MyTextField            Level3
+//(need update in MyTextField field to reflect at level3 and MyText)
